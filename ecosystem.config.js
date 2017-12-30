16b9e2a077602ef513a0a171a39b34e78eb7098f
zlav <zcjlavallee@gmail.com>
2017-12-29 22:23:00 -0600

    pm2 config added

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'Test Server',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'zach',
+      host: 'ec2-18-220-88-186.us-east-2.compute.amazonaws.com',
+      key: '~/.ssh/zRebelEC2',
+      ref: 'origin/master',
+      repo: 'git@github.com:zlav/TestServer',
+      path: '/home/zach/TestServer',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
