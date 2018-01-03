62f68a9bcd566df8e3d36f547c83f52edfdd0f46
Zach LaVallee <zcjlavallee@gmail.com>
2018-01-03 03:31:29 +0000

    index changed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('hello world 2')
+  res.send('hello world 2E')
 })
 
 app.listen(3300, '172.31.17.12' ,() => console.log('Server on port 3000'))
