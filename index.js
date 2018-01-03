7e9f64408e6f38b899c206b61b0a62934f4d6739
Zach LaVallee <zcjlavallee@gmail.com>
2018-01-03 06:19:56 +0000

    NPM Installed body parser for POST functionality

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('hello world 2E')
-})
+app.use(bodyParser.urlencoded({extended: true}))
 
+app.get('/', (req, res) => {
+  res.sendFile('/home/zach/TestServer/public' + '/index.html')
+})
+app.post('/quotes', (req, res) => {
+  console.log(req.body)
+  res.sendFile('/home/zach/TestServer/public' + '/index.html')
+})
 app.listen(3300, '172.31.17.12' ,() => console.log('Server on port 3000'))
