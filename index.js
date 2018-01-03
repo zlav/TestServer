c8009774180b9eac2d33553d4923e874937cd32c
Zach LaVallee <zcjlavallee@gmail.com>
2018-01-03 03:00:55 +0000

    index updated

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const app = express()
 
-app.use(express.static('public'))
+//app.use(express.static('public'))
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
 app.listen(3000, '172.31.17.12' ,() => console.log('Server on port 3000'))
